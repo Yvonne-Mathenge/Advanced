7a9f56ac9d541e140cec9038d658563b9108707a
Yvonne-Mathenge <yvonnewaithiram@gmail.com>
2025-04-07 12:09:42 +0300

    Create web.js

---
+++ web.js
@@ -0,0 +1,12 @@
+[
+    {
+        "title": "My First Blog Post",
+        "date": "2024-10-26",
+        "content": "This is my first blog post..."
+    },
+    {
+        "title": "Web Development Tips",
+        "date": "2024-11-01",
+        "content": "Some tips for web developers..."
+    }
+]
